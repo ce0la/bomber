b6ce0d57b71101fd94e5c530af07a909379eec47
Olaniyi Oshunbote <oshunboteo@gmail.com>
2024-12-05 21:13:13 +0100

    chore: Add logs path to .gitignore. Add telemetry code to http module.

--- lib/bomber/http.ex
+++ lib/bomber/http.ex
@@ -1,5 +1,23 @@
 defmodule Bomber.Http do
   def get(url) do
-    HTTPoison.get!(url)
+    :telemetry.execute(
+      [:http_request_started],
+      %{duration: 0},
+      %{url: url}
+    )
+
+    start_time = System.monotonic_time()
+
+    response = HTTPoison.get!(url)
+
+    duration = System.monotonic_time() - start_time
+
+    :telemetry.execute(
+      [:http_request_finished],
+      %{duration: duration},
+      %{url: url, status_code: response.status_code, success: :ok}
+    )
+
+    response
   end
 end

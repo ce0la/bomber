4c139bc24691674b49ae9431601f8315feb74a55
Olaniyi Oshunbote <oshunboteo@gmail.com>
2024-12-06 23:51:33 +0100

    refactor: take out telemetry and use File/inspect to write out logs

--- lib/bomber/http.ex
+++ lib/bomber/http.ex
@@ -1,23 +1,5 @@
 defmodule Bomber.Http do
   def get(url) do
-    :telemetry.execute(
-      [:http_request_started],
-      %{duration: 0},
-      %{url: url}
-    )
-
-    start_time = System.monotonic_time()
-
-    response = HTTPoison.get!(url)
-
-    duration = System.monotonic_time() - start_time
-
-    :telemetry.execute(
-      [:http_request_finished],
-      %{duration: duration},
-      %{url: url, status_code: response.status_code, success: :ok}
-    )
-
-    response
+    HTTPoison.get!(url)
   end
 end
--- lib/bomber/runner.ex
+++ lib/bomber/runner.ex
@@ -2,63 +2,19 @@ defmodule Bomber.Runner do
   require Logger
   require System
   require Task
-  import Bomber.Telemetry
+  import Bomber.Http
 
   def run(url, count) do
-    :telemetry.attach(
-      "log-request-handler",
-      [:http_request_started, :http_request_finished],
-      &Bomber.TelemetryHandler.handle_event/4,
-      nil
-    )
-
     tasks = Enum.map(1..count, fn _ ->
-      Task.async(fn -> execute_request(url) end)
+      Task.async(fn -> get(url) end)
     end)
 
     results = Task.await_many(tasks)
 
-    :telemetry.detach("log-request-handler")
+    File.open!("/home/ce0la/Documents/work/projects/bomber/logs/telemetry.log", [:write], fn file ->
+      IO.inspect(file, results, [])
+    end)
 
     {:ok, results}
-  end
-
-  defp execute_request(url) do
-    pid = self()
-
-    :telemetry.execute(
-      [:http_request_started],
-      %{duration: 0},
-      %{url: url}
-    )
-
-    # Log the start of the HTTP request
-    # http_request_started(pid, url)
-
-    # Track the start time for latency
-    start_time = System.monotonic_time()
-
-    try do
-      response = Bomber.Http.get(url)
-      duration = System.monotonic_time() - start_time
-      :telemetry.execute(
-        [:http_request_finished],
-        %{duration: duration},
-        %{url: url, status_code: response.status_code, success: :ok}
-      )
-
-      {:ok, url, response.status_code, duration}
-    rescue
-      e in HTTPoison.Error ->
-        duration = System.monotonic_time() - start_time
-        :telemetry.execute(
-          [:http_request_finished],
-          %{duration: duration},
-          %{url: url, status_code: nil, success: :error, error: e}
-        )
-        Logger.error("Request failed: #{inspect(e)}")
-
-        {:error, e, duration}
-    end
   end
 end
